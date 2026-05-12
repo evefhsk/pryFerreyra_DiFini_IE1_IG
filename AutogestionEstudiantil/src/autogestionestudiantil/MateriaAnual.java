3e977c76daafcbf6304a67ecf253b7c2e98937e5
eveferr <evvhopethv@gmail.com>
2026-05-12 15:06:21 -0300

    Update MateriaAnual.java

--- AutogestionEstudiantil/src/autogestionestudiantil/MateriaAnual.java
+++ AutogestionEstudiantil/src/autogestionestudiantil/MateriaAnual.java
@@ -10,7 +10,21 @@ package autogestionestudiantil;
  */
 public class MateriaAnual extends Materia
 {
+    private double notaPrimerCuatrimestre;
+    private double notaSegundoCuatrimestre;
 
+    private double asistenciaPrimerCuatrimestre;
+    private double asistenciaSegundoCuatrimestre;
+
+    public MateriaAnual(String nombre, String codigo, int anio) {
+
+        super(nombre, codigo, 0, anio);
+
+        this.notaPrimerCuatrimestre = 0;
+        this.notaSegundoCuatrimestre = 0;
+
+        this.asistenciaPrimerCuatrimestre = 0;
+        this.asistenciaSegundoCuatrimestre = 0;
 }
     
 
