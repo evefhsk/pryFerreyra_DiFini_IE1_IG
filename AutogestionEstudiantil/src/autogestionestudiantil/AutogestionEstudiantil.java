2cf8dda232137a14e8979c510b08f6b7fabfb44d
zairadifini123 <zairadifini123@gmail.com>
2026-05-12 19:18:34 -0300

    Objetos de prueba para MateriaCuatrimestral

--- AutogestionEstudiantil/src/autogestionestudiantil/AutogestionEstudiantil.java
+++ AutogestionEstudiantil/src/autogestionestudiantil/AutogestionEstudiantil.java
@@ -22,6 +22,36 @@ public class AutogestionEstudiantil
         alumno.inscribirse(m);
         alumno.inscribirse(a);
 
+        //Objetos de prueba Bonus: 
+        
+        //Probar funcionamiento BONUS (Clase MateriaCuatrimestral, Rankeable):
+        Estudiante e = new Estudiante("Zaira", "123", "Analista", 2026);
+        
+        //Objetos de prueba para la clase MateriaCuatrimestral
+        Materia m1 = new Materia("Matematica", "MAT1", 1, 2026);
+        Materia m2 = new MateriaCuatrimestral("Programacion", "PRO1", 1, 2026);
+
+        // Polimorfismo, las agrega en la lista
+        e.agregarMateria(m1);
+        e.agregarMateria(m2);
+
+        // Llama al metodo buscarPorCodigo de Estudiante
+        Materia buscada = e.buscarPorCodigo("PRO1");
+
+        //Muestra cual es
+        if (buscada != null) {
+            System.out.println("Materia encontrada: " + buscada.getNombre());
+        }
+        
+        //Inscribe la materia 
+        InscripcionMateria ins = new InscripcionMateria(m2);
+        ins.getNotas().add(8.0); //Agrega notas manualmente
+        ins.getNotas().add(7.0);
+        
+        //Calcula sobre esas notas el puntake ranking
+        System.out.println("Puntaje ranking: " + ins.getPuntajeRanking());
+
+        
         
         int opcion;
 
