0ac586e1f836154c8b51871806049a29ba0917b7
zairadifini123 <zairadifini123@gmail.com>
2026-05-12 19:28:28 -0300

    Revert "Merge branch 'feuture/menu' of https://github.com/evefhsk/pryFerreyra_DiFini_IE1_IG into feuture/menu"
    This reverts commit d07b32d40a0e99cd3e6605b018793992473ab536, reversing
    changes made to 1cec73a7360053dbddcfd615bd19629f15ac273f.

--- AutogestionEstudiantil/src/autogestionestudiantil/Estudiante.java
+++ AutogestionEstudiantil/src/autogestionestudiantil/Estudiante.java
@@ -106,7 +106,6 @@ public class Estudiante extends PersonaAcademica implements Consultable{
         }
 
         return buscarRecursivo(codigo, i + 1);
-    } 
 
     public double getPromedioGeneral() {
 
