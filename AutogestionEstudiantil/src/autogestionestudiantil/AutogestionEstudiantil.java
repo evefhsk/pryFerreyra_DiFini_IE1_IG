357562898b92fd45ddf46ce0812ca7cd0a8643fc
eveferr <evvhopethv@gmail.com>
2026-05-13 23:57:58 -0300

    Update AutogestionEstudiantil.java

--- AutogestionEstudiantil/src/autogestionestudiantil/AutogestionEstudiantil.java
+++ AutogestionEstudiantil/src/autogestionestudiantil/AutogestionEstudiantil.java
@@ -20,49 +20,7 @@ public class AutogestionEstudiantil
         Materia m = new Materia("Matematica", "22033", 1, 2024);
         Materia a = new Materia("Lengua", "22034", 2, 2024);
         
-        System.out.println("====Inscripción de materias de prueba===="); 
-   
-        alumno.inscribirse(m);
-        alumno.inscribirse(a);
-      
-        //Objetos de prueba Bonus: 
-        
-        //Probar funcionamiento BONUS (Clase MateriaCuatrimestral, Rankeable):
-        Estudiante e = new Estudiante("Zaira", "123", "Analista", 2026);
-        
-        //Objetos de prueba para la clase MateriaCuatrimestral
-        Materia m1 = new Materia("Matematica", "MAT1", 1, 2026);
-        Materia m2 = new MateriaCuatrimestral("Programacion", "PRO1", 1, 2026);
-        System.out.println();
-        
-        System.out.println("====Materias agregadas con polimorfismo====");
-        
-        // Polimorfismo, las agrega en la lista
-        e.agregarMateria(m1);
-        e.agregarMateria(m2);
-        
-        System.out.println(); 
-        
-        System.out.println("Búsqueda por código");
-        // Llama al metodo buscarPorCodigo de Estudiante
-        Materia buscada = e.buscarPorCodigo("PRO1");
-
-        //Muestra cual es
-        if (buscada != null) {
-            System.out.println("Materia encontrada: " + buscada.getNombre());
-        }
-        
-        //Inscribe la materia 
-        InscripcionMateria ins = new InscripcionMateria(m2);
-        ins.getNotas().add(8.0); //Agrega notas manualmente
-        ins.getNotas().add(7.0);
-        
-        //Calcula sobre esas notas el puntake ranking
-        System.out.println("Puntaje ranking: " + ins.getPuntajeRanking());
-
-         
-        
-        int opcion;
+       
 
         // MENU PRINCIPAL (SOLO ESTRUCTURA INICIAL)
         do {
